ae7d2501c13a81a7cacd0c78ea0a60cbb5588858
bangkitwahyus <bangkitwahyusaputra@gmail.com>
2022-01-12 21:26:24 +0700

    install cors dan nodemon

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 var morgan = require('morgan');
+var cors = require('cors');
 
 // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(cors());
 
 // panggil routes
 var routes = require('./routes');
@@ -16,6 +18,6 @@ routes(app);
 app.use('/auth',require('./middleware'));
 
 
-app.listen(3000, () => {
-    console.log(`Server started on port 3000`);
+app.listen(3001, () => {
+    console.log(`Server started on port 3001`);
 });
