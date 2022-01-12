7c49a114643374cb9903af2a42d40bb35f4a0b0a
bangkitwahyus <bangkitwahyusaputra@gmail.com>
2022-01-12 19:59:17 +0700

    meanmbahkan fungsi registrasi dan index.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -18,28 +18,28 @@ exports.registrasi=function(req, res){
         tanggal_daftar: new Date(),
     }
 
-    var query = "SELECT email FROM ?? WHERE ?";
+    var query = "SELECT email FROM ?? WHERE ??=?";
     var table = ["user","email", post.email];
 
     query = mysql.format(query, table);
 
-    connection.query(query, function(err, rows){
+    connection.query(query, function(error, rows){
         if(error){
-            console.log(err);
+            console.log(error);
         }else{
             if(rows.length == 0){
                 var query = "INSERT INTO ?? SET ?";
                 var table = ["user"];
                 query = mysql.format(query, table);
-                connection.query(query, post,function(err, rows){
+                connection.query(query, post,function(error, rows){
                     if(error){
-                        console.log(err);
+                        console.log(error);
                     }else{
-                        response.ok('Berhasil Menambahkan data user baru',res);
+                        response.ok('Berhasil menambahkan data user baru',res);
                     }
-                })
+                });
             }else{
-                response.ok('Email sudah terdaftar');
+                response.ok('Email sudah terdaftar!',res);
             }
         }
     })
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+var morgan = require('morgan');
 
 // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // panggil routes
 var routes = require('./routes');
 routes(app);
+
+// Daftarkan menu routes dari index
+app.use('/auth',require('./middleware'));
 
 
 app.listen(3000, () => {
